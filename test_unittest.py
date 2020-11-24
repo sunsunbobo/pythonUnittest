beb773136a7854c7daa5101e2b1d9e508930d8ce
sunsunbobo <sunbo_2010@foxmail.com>
2020-11-24 16:59:19 +0800

    first commit unittest

---
+++ test_unittest.py
@@ -0,0 +1,28 @@
+import unittest
+
+
+class DemoUnittest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        print("setup")
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def tearDown(self) -> None:
+        print("tearDown")
+
+    # def test_split(self):
+    #     s: str = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
